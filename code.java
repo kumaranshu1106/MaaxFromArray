3bd81a4d9e6641072e3718a461d337f1d61ea219
susu321jha <115914148+susu321jha@users.noreply.github.com>
2022-10-16 11:47:42 +0530

    Update code.java

--- code.java
+++ code.java
@@ -3,7 +3,7 @@ public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter the number of element you want to store: ");
 int n = sc.nextInt();
-
+int array[]=new int[10];
  System.out.println("Enter element for Array: ");
 for(int i=0; i<n; i++){
 array[i]=sc.nextInt();
